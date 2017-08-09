a15200da4983555f69bafd0688363e7a523f4a7e
ESTUDIANTE <ESTUDIANTE@453-203-1>
2017-08-09 14:04:22 -0500

    Practica

---
+++ src/clase/pkg3/Clase3.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase.pkg3;
+import java.util.*;
+/**
+ *
+ * @author ESTUDIANTE
+ * @version 1
+ * @since 9 de agosto del 2017 
+ */
+
+
+public class Clase3 {
+public static double calcularsalario (int horas, double valor){
+     double ac;
+       if ( horas <= 40){
+           ac = horas * valor;
+       } else {
+           if (horas >40 && horas <= 48 ) {
+               int d= horas - 40;
+               ac= (40*valor) + 2*valor*d;
+           } else {
+               int a = horas-48;
+               int d= horas - a- 40;
+               ac= (40*valor) + 2*valor*d + 3*valor*a;
+           }
+       }
+    return ac ;
+
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner flujoEnt = new Scanner (System.in);
+        String nombre = null;
+        int numHoras = 0;
+        double ValorHora= 0;
+        double valorPagar =0;
+        
+        // Lectura de datos
+        System.out.println ("Nombre:");
+        nombre = flujoEnt.next ();
+        
+        System.out.println ("Horas Trabajadas:");
+        numHoras = flujoEnt.nextInt ();
+        
+        System.out.println ("Valor Hora:");
+        ValorHora = flujoEnt.nextDouble ();
+         //invocacion de la funcion
+        valorPagar = calcularsalario (numHoras, ValorHora);
+        
+        System.out.println ( "nombre: " + nombre + ": Salario total " + valorPagar);
+        
+        
+    }
+    
+}
